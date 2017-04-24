d338221049182d3c7eab78db12bfbf6064376342
qflz1992 <18215628861@163.com>
2017-04-24 16:43:01 +0800

    提交公司配置

--- init.el
+++ init.el
@@ -1,36 +1,120 @@
 
 ;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
+;; installed packages.  Don't delete this line.  If you don't want (setq  initial-frame-alist (quote ((fullscreen . maximized))))it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
 
-;; 关闭顶部工具栏
+(when (>= emacs-major-version 24)
+    (require 'package)
+    (package-initialize)
+    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+    )
+
+(require 'cl)
+
+;;add whatever packages you want here
+(defvar zilongshanren/packages '(
+				   company
+				   monokai-theme
+				   hungry-delete
+				   swiper
+				   counsel
+				   smartparens
+				   js2-mode
+				   nodejs-repl
+				   exec-path-from-shell
+				   
+				   )  "Default packages")
+
+(setq package-selected-packages zilongshanren/packages)
+
+(defun zilongshanren/packages-installed-p ()
+    (loop for pkg in zilongshanren/packages
+          when (not (package-installed-p pkg)) do (return nil)
+          finally (return t)))
+
+(unless (zilongshanren/packages-installed-p)
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg zilongshanren/packages)
+      (when (not (package-installed-p pkg))
+        (package-install pkg))))
+
+;; let emacs could find the execuable
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+;; config js2-mode for js files
+(setq auto-mode-alist
+	  (append
+	   '(("\\.js\\'" . js2-mode))
+	   auto-mode-alist))
+
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+
 (tool-bar-mode -1)
-;; 关闭右侧滚动条
 (scroll-bar-mode -1)
-;; 
-(electric-indent-mode -1)
-;; 显示行号
-(global-linum-mode 1)
-;; 打开自动补全
-(global-company-mode t)
-;; 关闭开始帮助页面
+
 (setq inhibit-splash-screen t)
-;; 定义一个函数,打开配置文件
+
+(global-linum-mode t)
+
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
-;; 绑定打开配置文件的快捷键
+
 (global-set-key (kbd "<f2>") 'open-my-init-file)
-;; 设置光标为bar
+
+(global-company-mode t)
 (setq-default cursor-type 'bar)
-;; 背景颜色
-;;(set-background-color "gray30")
-;; 默认的环境编码
-(set-language-environment "utf-8")
+
+(setq make-backup-files nil)
 
 
+(require 'org)
+
+(setq org-src-fontify-natively t)
+
+
+(require 'recentf)
+(recentf-mode 1)			
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(delete-selection-mode t)
+
+(setq  initial-frame-alist (quote ((fullscreen . maximized))))
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(global-hl-line-mode t)
+
+(load-theme 'monokai t)
+
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+ (setq org-agenda-files '("~/org"))
+  (global-set-key (kbd "C-c a") 'org-agenda)
+  
+  
 ;; ============================================================
 ;; Setting English Font
 (set-face-attribute
@@ -41,32 +125,25 @@
             charset
             (font-spec :family "Microsoft Yahei" :size 16)))
 
-;; 缩进设置
+;; 配置缩进
 (setq indent-tabs-mode nil)
 (setq default-tab-width 4)
 (setq tab-width 4)
-
-;; 关闭备份文件
-(setq make-backup-files nil)
-
-;; 打开最近编辑的文件
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-item 10)
-
-
-
-
+  
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   (quote
+    ("6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(js2-external-variable ((t (:foreground "dark gray")))))
